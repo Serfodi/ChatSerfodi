3be3edafeb2e8fa17237849d470af3f8553b2128
Serfodi <nasybullin8@gmail.com>
2023-10-21 13:48:46 +0300

    Initial Commit

---
+++ ChatSerfodi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatSerfodi
+//
+//  Created by Сергей Насыбуллин on 21.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
